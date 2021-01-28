19862b52cf23215ff86e2f375a3a687c5f169990
Qifa Wang <qifwang@umich.edu>
2021-01-28 10:41:41 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//Project Identifier: B99292359FFD910ED13A7E6C7F9705B8742F0D79
+#include <iostream>
+#include <stdlib>
+#include <vector>
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
